8d2247e9e2823204a01fc32e8ae7a51a1d5d0cac
Rene Rotvig  Jensen <renerotvig@msn.com>
2019-02-14 10:25:36 +0100

    Initial API

---
+++ DanishRegisterOfMotorVehicles/Controllers/CarInformationController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using Dmr;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DanishRegisterOfMotorVehicles.Api.Controllers
+{
+    public class CarInformationController : Controller
+    {
+        [HttpGet("api/carinformation/{numberplate}")]
+        public async Task<string> GetCarInformation(string numberplate)
+        {
+            var model = Dmr.Scraper.LookupVehicle(numberplate);
+            var token = model.Token;
+            return model.ToJson();
+        }
+    }
+}
